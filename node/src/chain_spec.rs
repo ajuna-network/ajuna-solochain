bb24d5731860abf15e8c46ddb3f34857826d207e
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-03-06 11:31:49 +0800

    rustfmt

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -47,25 +47,25 @@ pub fn development_config() -> Result<ChainSpec, String> {
 		WASM_BINARY.ok_or_else(|| "WASM binary was not built, please build it!".to_string())?,
 		None,
 	)
-		.with_name("Ajuna Dev Testnet")
-		.with_id("ajuna-dev-testnet")
-		.with_properties(properties)
-		.with_chain_type(ChainType::Development)
-		.with_genesis_config_patch(testnet_genesis(
-			// Initial PoA authorities
-			vec![authority_keys_from_seed("Alice")],
-			// Sudo account
+	.with_name("Ajuna Dev Testnet")
+	.with_id("ajuna-dev-testnet")
+	.with_properties(properties)
+	.with_chain_type(ChainType::Development)
+	.with_genesis_config_patch(testnet_genesis(
+		// Initial PoA authorities
+		vec![authority_keys_from_seed("Alice")],
+		// Sudo account
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		// Pre-funded accounts
+		vec![
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-			],
-			true,
-		))
-		.build())
+			get_account_id_from_seed::<sr25519::Public>("Bob"),
+			get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+		],
+		true,
+	))
+	.build())
 }
 
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
@@ -78,33 +78,33 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
 		None,
 	)
-		.with_name("Ajuna Testnet")
-		.with_id("ajuna-testnet")
-		.with_properties(properties)
-		.with_chain_type(ChainType::Local)
-		.with_genesis_config_patch(testnet_genesis(
-			// Initial PoA authorities
-			vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
-			// Sudo account
+	.with_name("Ajuna Testnet")
+	.with_id("ajuna-testnet")
+	.with_properties(properties)
+	.with_chain_type(ChainType::Local)
+	.with_genesis_config_patch(testnet_genesis(
+		// Initial PoA authorities
+		vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
+		// Sudo account
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		// Pre-funded accounts
+		vec![
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie"),
-				get_account_id_from_seed::<sr25519::Public>("Dave"),
-				get_account_id_from_seed::<sr25519::Public>("Eve"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-			],
-			true,
-		))
-		.build())
+			get_account_id_from_seed::<sr25519::Public>("Bob"),
+			get_account_id_from_seed::<sr25519::Public>("Charlie"),
+			get_account_id_from_seed::<sr25519::Public>("Dave"),
+			get_account_id_from_seed::<sr25519::Public>("Eve"),
+			get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+			get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+			get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+		],
+		true,
+	))
+	.build())
 }
 
 /// Configure initial storage state for FRAME modules.
