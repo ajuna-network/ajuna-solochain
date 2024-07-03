fb4b1c6b6cb7d562aa59cfd5bbbfcb341555a1b0
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-07-03 11:55:44 +0200

    add `CheckMetadataHash` signed extension

--- runtime/solo/build.rs
+++ runtime/solo/build.rs
@@ -14,16 +14,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#[cfg(feature = "std")]
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
-	substrate_wasm_builder::WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
+	substrate_wasm_builder::WasmBuilder::build_using_defaults()
+}
+
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("AJUN", 12)
 		.build()
 }
 
-/// The wasm builder is deactivated when compiling
-/// this crate for wasm to speed up the compilation.
 #[cfg(not(feature = "std"))]
 fn main() {}
